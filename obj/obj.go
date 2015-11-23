11517eda225de104bfef7f3931434f833e7c9872
Angus Gibson <github@angus.agibson.me>
2015-11-24 10:08:47 +1100

    Preliminary parsing into vertices and elements
    We just parse out the vertices and the vertex index portion of elements
    from an OBJ file, and return them to the caller. This is almost all we
    need to do, other than enhancing robustness and also processing normal
    and texture coordinate data.

---
+++ obj/obj.go
@@ -0,0 +1,57 @@
+// Package obj allows simple parsing of Wavefront OBJ files
+// into slices of vertices, normals and elements.
+package obj
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Parse a vertex (normal) string, a list of whitespace-separated
+// floating point numbers.
+func parseVertex(t []string) []float32 {
+	x, _ := strconv.ParseFloat(t[0], 32)
+	y, _ := strconv.ParseFloat(t[1], 32)
+	z, _ := strconv.ParseFloat(t[2], 32)
+
+	return []float32{float32(x), float32(y), float32(z)}
+}
+
+// Parse an element string, a list of whitespace-separated elements.
+// Elements are of the form "<vi>/<ti>/<ni>" where indices are the
+// vertex, texture coordinate and normal, respectively.
+func parseElement(t []string) []int32 {
+	e := make([]int32, len(t))
+
+	for i := 0; i < len(t); i++ {
+		f := strings.Split(t[i], "/")
+		// for now, just grab the vertex index
+		x, _ := strconv.ParseInt(f[0], 10, 32)
+		e[i] = int32(x) - 1
+	}
+
+	return e
+}
+
+func Parse(filename string) ([]float32, []int32) {
+	fp, _ := os.Open(filename)
+	scanner := bufio.NewScanner(fp)
+
+	vertices := []float32{}
+	elements := []int32{}
+
+	for scanner.Scan() {
+		toks := strings.Fields(strings.TrimSpace(scanner.Text()))
+
+		switch toks[0] {
+		case "v":
+			vertices = append(vertices, parseVertex(toks[1:])...)
+		case "f":
+			elements = append(elements, parseElement(toks[1:])...)
+		}
+	}
+
+	return vertices, elements
+}
